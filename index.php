3fe988bc5e0cd1fb651ec9b98cd433788e90ee5b
Brunno de Souza Oliveira <110069192+BrunnoSouzaOliveira@users.noreply.github.com>
2022-08-04 22:26:00 -0300

    add bootstrap e atualização do index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Good morning vietnam";
+function dd($input){
+    var_dump($input);
+    die();
+}
+
+$mainPosition = __DIR__;
+
+require("{$mainPosition}\bootstrap\Env.php");
+
+$env = new Env();
+dd($env);
